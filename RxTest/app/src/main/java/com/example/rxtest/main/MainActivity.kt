3376fd40f967ef5686c5e93bda0413c85d561a31
JooHeonLee <jooheon.lee@kt.com>
2021-11-11 10:19:53 +0900

    CHANGED: MainActivity -> RepositoryFragment로 기능 옮기는 작업 ADDED: BaseFragment 클래스 추가

--- RxTest/app/src/main/java/com/example/rxtest/main/MainActivity.kt
+++ RxTest/app/src/main/java/com/example/rxtest/main/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.example.rxtest.main
 
 import android.os.Bundle
-import androidx.lifecycle.Observer
 import com.example.rxtest.R
 import com.example.rxtest.base.BaseActivity
 import com.example.rxtest.databinding.ActivityMainBinding
 import com.jakewharton.rxbinding4.view.clicks
-import com.jakewharton.rxbinding4.widget.textChanges
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.schedulers.Schedulers
 import java.util.concurrent.TimeUnit
 
 class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
@@ -22,25 +19,18 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
     }
 
     override fun initDataBinding() {
-        viewModel.repositoryLiveData.observe(this, Observer {
-            viewDataBinding.tvRepository.setText(it);
-        })
-
         // 버튼 클릭 리스너 observable
         viewDataBinding.btRepository.clicks()
             .throttleFirst(1, TimeUnit.SECONDS, AndroidSchedulers.mainThread()) // avoid multi click
             .subscribe {
-                val owner = viewDataBinding.etRepository.text.toString()
-                viewModel.callRepositoryApi(owner)
+                viewModel.replaceRepositoryFragment(supportFragmentManager)
             }
 
-        // EditText Observable, 1초에 한번 Trigger 된다.
-        val etRepositoryObservable = viewDataBinding.etRepository.textChanges()
-        etRepositoryObservable.debounce(1000, TimeUnit.MILLISECONDS)
-            .subscribeOn(Schedulers.io())
-            .subscribe({
-                viewModel.callRepositoryApi(it.toString())
-            })
+        viewDataBinding.btCompose.clicks()
+            .throttleFirst(1, TimeUnit.SECONDS, AndroidSchedulers.mainThread())
+            .subscribe {
+                viewModel.replaceComposeFragment(supportFragmentManager)
+            }
     }
 
     override fun initAfterBinding() {
--- RxTest/app/src/main/java/com/example/rxtest/main/MainViewModel.kt
+++ RxTest/app/src/main/java/com/example/rxtest/main/MainViewModel.kt
@@ -1,9 +1,14 @@
 package com.example.rxtest.main
 
-import android.util.Log
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import androidx.lifecycle.MutableLiveData
+import com.example.rxtest.R
 import com.example.rxtest.api.github.GithubClient
 import com.example.rxtest.base.BaseViewModel
+import com.example.rxtest.compose.ComposeFragment
+import com.example.rxtest.compose.RepositoryFragment
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.rxkotlin.addTo
 import io.reactivex.rxkotlin.subscribeBy
@@ -12,28 +17,21 @@ import io.reactivex.schedulers.Schedulers;
 class MainViewModel : BaseViewModel(){
     private val TAG = MainViewModel::class.simpleName
 
-    var repositoryLiveData = MutableLiveData<String>()
-
     init {
         // do something..
     }
 
-    fun callRepositoryApi(owner: String) {
-        GithubClient.getClient().getRepos(owner)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribeBy(
-                onSuccess = {
-//                    repositoryLiveData.postValue(it.toString()) // BackgroundThread에서 돌다가 main이 게시될때 값을 전달한다.
-                    repositoryLiveData.value = it.toString() // MainThread에서 실행해야 한다. (이 rx는 consumer가 mainThread이기때문에 이렇게 해도 됨)
-                },
-                onError =  {
-                    it.message?.let {
-                        repositoryLiveData.value = it
-                    } ?: run {
-                        repositoryLiveData.value = "error @@@@"
-                    }
-                }
-            ).addTo(compositeDisposable)
+    fun replaceComposeFragment(supportFragmentManager: FragmentManager) {
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            replace<ComposeFragment>(R.id.fragment_container_view)
+        }
+    }
+
+    fun replaceRepositoryFragment(supportFragmentManager: FragmentManager) {
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            replace<RepositoryFragment>(R.id.fragment_container_view)
+        }
     }
 }
