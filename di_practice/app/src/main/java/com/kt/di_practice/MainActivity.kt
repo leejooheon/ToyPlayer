a571ada4974abca107afb127ced225158e83188f
JooHeonLee <jooheon.lee@kt.com>
2021-12-20 16:36:16 +0900

    - Field Injection

--- di_practice/app/src/main/java/com/kt/di_practice/MainActivity.kt
+++ di_practice/app/src/main/java/com/kt/di_practice/MainActivity.kt
@@ -2,17 +2,28 @@ package com.kt.di_practice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kt.di_practice.car.Car
 import com.kt.di_practice.di.CarComponent
 import com.kt.di_practice.di.DaggerCarComponent
+import kotlinx.android.synthetic.main.activity_main.*
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+    @Inject
+    lateinit var car: Car
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val carComponent = DaggerCarComponent.create();
-        val car = carComponent.car
+        carComponent.inject(this)
+    }
 
-        car.drive()
+    override fun onResume() {
+        super.onResume()
+        bt_start.setOnClickListener { v ->
+            car.drive()
+        }
     }
 }
--- di_practice/app/src/main/java/com/kt/di_practice/di/CarComponent.kt
+++ di_practice/app/src/main/java/com/kt/di_practice/di/CarComponent.kt
@@ -1,10 +1,11 @@
 package com.kt.di_practice.di
 
+import com.kt.di_practice.MainActivity
 import com.kt.di_practice.car.Car
 import dagger.Component
 
 
 @Component
 interface CarComponent {
-    val car: Car
+    fun inject(activity:MainActivity)
 }
