b749d86807bc804ac2394f66520f87b6985d0826
JooHeonLee <jooheon.lee@kt.com>
2022-03-03 12:02:07 +0900

    UPDATE: Splash 화면 preview 기능

--- RxTest/presentation/src/main/java/com/jooheon/clean_architecture/presentation/view/splash/SplashScreen.kt
+++ RxTest/presentation/src/main/java/com/jooheon/clean_architecture/presentation/view/splash/SplashScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -23,7 +22,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.jooheon.clean_architecture.presentation.R
-import com.jooheon.clean_architecture.presentation.theme.SplashTheme
 import com.jooheon.clean_architecture.presentation.view.destinations.MainScreenDestination
 import com.jooheon.clean_architecture.presentation.view.destinations.SplashScreenDestination
 import com.ramcosta.composedestinations.annotation.Destination
@@ -34,57 +32,55 @@ import com.ramcosta.composedestinations.navigation.EmptyDestinationsNavigator
 @Composable
 fun SplashScreen(
     navigator: DestinationsNavigator,
-    viewModel: SplashViewModel = hiltViewModel()
+    viewModel: SplashViewModel = hiltViewModel(),
+    scaleInitialValue: Float = 0f
 ) {
+    val scale = remember { Animatable(scaleInitialValue) }
 
-    SplashTheme() {
-        val scale = remember { Animatable(0f) }
-
-        LaunchedEffect(true) {
-            scale.animateTo(
-                targetValue = 0.7f,
-                animationSpec = tween(
-                    durationMillis = 800,
-                    easing = {
-                        OvershootInterpolator(4f).getInterpolation(it)
-                    }
-                )
+    LaunchedEffect(true) {
+        scale.animateTo(
+            targetValue = 0.85f,
+            animationSpec = tween(
+                durationMillis = 800,
+                easing = {
+                    OvershootInterpolator(4f).getInterpolation(it)
+                }
             )
-        }
-
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color.DarkGray),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.ic_baseline_toys_24),
-                contentDescription = null,
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .scale(scale.value)
-                    .size(150.dp)
-            )
-
-            Text(
-                modifier = Modifier
-                    .padding(top = 6.dp)
-                    .scale(scale.value),
-                text = "My Toy Project",
-                color = Color.White,
-                fontWeight = FontWeight.Bold
-            )
-        }
-
-        PrepareRecomposableHandler(navigator, viewModel)
+        )
     }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.DarkGray),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_baseline_toys_24),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .scale(scale.value)
+                .size(150.dp)
+        )
+
+        Text(
+            modifier = Modifier
+                .padding(top = 6.dp)
+                .scale(scale.value),
+            text = "My Toy Project",
+            color = Color.White,
+            fontWeight = FontWeight.Bold
+        )
+    }
+
+    PrepareRecomposableHandler(navigator, viewModel)
 }
 
 
 @Composable
-fun PrepareRecomposableHandler(
+internal fun PrepareRecomposableHandler(
     navigator: DestinationsNavigator,
     viewModel: SplashViewModel
 ) {
@@ -133,7 +129,6 @@ fun PrepareRecomposableHandler(
 @Preview
 @Composable
 fun PreviewSplashScreen() {
-    Button(onClick = {}) {
-        Text(text = "test")
-    }
+    val viewModel = SplashViewModel()
+    SplashScreen(EmptyDestinationsNavigator, viewModel, 0.85f)
 }
