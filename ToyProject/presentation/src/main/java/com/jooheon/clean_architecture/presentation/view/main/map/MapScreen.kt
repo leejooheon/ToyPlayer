7f0f3d431a181c3c012c79a800734edd1824495b
Jooheon <jooheon.lee@nhnsoft.com>
2022-07-15 17:10:38 +0900

    ADDED: exoplayer 테스트 추가

--- ToyProject/presentation/src/main/java/com/jooheon/clean_architecture/presentation/view/main/map/MapScreen.kt
+++ ToyProject/presentation/src/main/java/com/jooheon/clean_architecture/presentation/view/main/map/MapScreen.kt
@@ -1,12 +1,21 @@
 package com.jooheon.clean_architecture.presentation.view.main.map
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
+import android.Manifest
+import android.util.Log
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.google.accompanist.permissions.*
+import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
@@ -14,12 +23,22 @@ import com.google.maps.android.compose.CameraPositionState
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.Marker
+import com.jooheon.clean_architecture.domain.entity.Entity
+import com.jooheon.clean_architecture.presentation.theme.themes.PreviewTheme
 import com.jooheon.clean_architecture.presentation.utils.ObserveAlertDialogState
 import com.jooheon.clean_architecture.presentation.utils.ObserveLoadingState
+import com.jooheon.clean_architecture.presentation.utils.ShowAlertDialog
+import com.jooheon.clean_architecture.presentation.utils.UiText
 import com.jooheon.clean_architecture.presentation.view.main.MainViewModel
+import com.jooheon.clean_architecture.presentation.view.temp.EmptyGithubUseCase
+import com.jooheon.clean_architecture.presentation.view.temp.EmptyParkingSpotUseCase
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.ramcosta.composedestinations.navigation.EmptyDestinationsNavigator
+import kotlinx.coroutines.launch
 
+private val DEFAULT_LATLNG:LatLng = LatLng(37.5033311460182, 126.94775238633156)
 
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun MapScreen(
     navigator: DestinationsNavigator,
@@ -27,69 +46,169 @@ fun MapScreen(
     viewModel: MapViewModel = hiltViewModel(),
     isPreview: Boolean = false,
 ) {
-    val uiSettings = remember {
-        MapUiSettings(
-            zoomControlsEnabled = true
-        )
+    val uiSettings = remember { MapUiSettings(zoomControlsEnabled = true) }
+
+    val cameraPositionState = remember { CameraPositionState(
+        CameraPosition(DEFAULT_LATLNG, 17f, 0f, 0f))
     }
 
-    val cameraPositionState = CameraPositionState(
-        CameraPosition(LatLng(37.5033311460182, 126.94775238633156), 17f, 0f, 0f)
+    val permissionState = rememberPermissionState(
+        permission = Manifest.permission.ACCESS_FINE_LOCATION
     )
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background)
-    ) {
+    Box(modifier = Modifier.fillMaxSize()) {
         GoogleMap(
             modifier = Modifier.fillMaxSize(),
-            properties = viewModel.state.properties,
+            properties = viewModel.mapState.properties.value,
             uiSettings = uiSettings,
             cameraPositionState = cameraPositionState,
             onMapLongClick = {
                 viewModel.onEvent(MapEvent.OnMapLongClick(it))
+            },
+            onMyLocationClick = {
+                Log.d("Jooheon", "onMyLocationButtonClick")
             }
         ) {
-            val parkingSpots = viewModel.state.parkingSpots.value
-            parkingSpots?.forEach { spot ->
-                Marker(
-                    position = LatLng(spot.lat, spot.lng),
-                    title = "Parking spot (${spot.lat}, ${spot.lng})",
-                    snippet = "Long click to delete",
-                    onInfoWindowLongClick = {
-                        viewModel.onEvent(
-                            MapEvent.OnInfoWindowLongClick(spot)
-                        )
-                    },
-                    onClick = {
-                        it.showInfoWindow()
-                        true
-                    },
-                    icon = BitmapDescriptorFactory.defaultMarker(
-                        BitmapDescriptorFactory.HUE_GREEN
-                    )
-                )
+            val parkingSpots = viewModel.mapState.parkingSpots.value
+            parkingSpots?.forEach {
+                ParkingSpotMarker(viewModel, it)
             }
         }
+
+        PermissionButton(viewModel, permissionState)
     }
 
-    if(!isPreview) {
-        observeFloatingActionEvent(sharedViewModel, viewModel)
-    }
+    ObserveEvents(sharedViewModel, viewModel, permissionState)
+    ObserveDialogStates(viewModel, permissionState)
 
     ObserveAlertDialogState(viewModel)
-    ObserveLoadingState(viewModel)
+}
+
+@ExperimentalPermissionsApi
+@Composable
+private fun PermissionButton(
+    viewModel: MapViewModel,
+    permissionState: PermissionState
+) {
+    if(permissionState.status.isGranted) {
+        return
+    }
+
+    Button(
+        onClick = {
+            viewModel.onFindMyLocationButtonClicked(permissionState)
+        },
+        colors = ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.secondary,
+            contentColor = MaterialTheme.colorScheme.onSecondary
+        )
+    ) {
+        Text(
+            text = "Need permission",
+            style = MaterialTheme.typography.bodySmall
+        )
+    }
 }
 
 @Composable
-fun observeFloatingActionEvent(
+private fun ParkingSpotMarker(
+    viewModel: MapViewModel,
+    spot: Entity.ParkingSpot
+) {
+    val latitude = viewModel.decimalFormat.format(spot.lat)
+    val longitude = viewModel.decimalFormat.format(spot.lng)
+    Marker(
+        position = LatLng(spot.lat, spot.lng),
+        title = "Spot (${latitude}, ${longitude})",
+        snippet = "Long click to delete",
+        onInfoWindowLongClick = { viewModel.onEvent(MapEvent.OnInfoWindowLongClick(spot)) },
+        onClick = {
+            it.showInfoWindow()
+            true
+        },
+        icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)
+    )
+}
+
+@ExperimentalPermissionsApi
+@Composable
+private fun ObserveDialogStates(
+    viewModel: MapViewModel,
+    permissionState: PermissionState
+) {
+    val askPermissionState = viewModel.permissionChannel.collectAsState(initial = null).value
+    askPermissionState?.let {
+        ShowAlertDialog(
+            openDialog = mutableStateOf(true),
+            content = UiText.DynamicString("Need permission\nfor find your location"),
+            viewModel = viewModel,
+            onOkButtonClicked = {
+                viewModel.dismissAlertDialog()
+                permissionState.launchPermissionRequest()
+            }
+        )
+    }
+
+    val insertDialogState = viewModel.insertDialogChannel.collectAsState(initial = null).value
+    insertDialogState?.let {
+        val content = UiText.DynamicString("Add this spot?")
+        ShowAlertDialog(
+            openDialog = mutableStateOf(true),
+            content = content,
+            viewModel = viewModel,
+            onOkButtonClicked = {
+                viewModel.insertParkingSpot(it)
+            }
+        )
+    }
+}
+
+@ExperimentalPermissionsApi
+@Composable
+private fun ObserveEvents(
     sharedViewModel: MainViewModel,
     viewModel: MapViewModel,
+    permissionState: PermissionState
 ) {
-    LaunchedEffect(Unit) {
-        sharedViewModel.floatingActionClicked.collect {
-            viewModel.onEvent(MapEvent.ToggleFalloutMap)
+    val lifecycleOwner by rememberUpdatedState(LocalLifecycleOwner.current)
+    DisposableEffect(
+        key1 = lifecycleOwner,
+        effect = {
+            val observer = LifecycleEventObserver { lifecycleOwner, event ->
+                lifecycleOwner.lifecycleScope.launch {
+                    lifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                        sharedViewModel.floatingActionClicked.collect {
+                            viewModel.onEvent(MapEvent.ToggleFalloutMap)
+                        }
+                    }
+                }
+
+                if(event == Lifecycle.Event.ON_RESUME) {
+                    viewModel.onEvent(
+                        MapEvent.OnLocationPermissionChanged(permissionState.status.isGranted)
+                    )
+                }
+            }
+
+            lifecycleOwner.lifecycle.addObserver(observer)
+
+            onDispose {
+                lifecycleOwner.lifecycle.removeObserver(observer)
+            }
         }
+    )
+}
+
+@Preview
+@Composable
+private fun PreviewMapScreen() {
+    val viewModel = MapViewModel(EmptyParkingSpotUseCase())
+    PreviewTheme(true) {
+        MapScreen(
+            navigator = EmptyDestinationsNavigator,
+            sharedViewModel = MainViewModel(EmptyGithubUseCase()),
+            viewModel = viewModel,
+            isPreview = true
+        )
     }
 }
--- ToyProject/presentation/src/main/java/com/jooheon/clean_architecture/presentation/view/main/search/SearchScreen.kt
+++ ToyProject/presentation/src/main/java/com/jooheon/clean_architecture/presentation/view/main/search/SearchScreen.kt
@@ -1,24 +1,185 @@
 package com.jooheon.clean_architecture.presentation.view.main.search
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import android.content.Context
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.min
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.MediaMetadata
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.ui.StyledPlayerView
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
+import com.google.android.exoplayer2.util.Util
+import com.jooheon.clean_architecture.presentation.R
+import com.jooheon.clean_architecture.presentation.theme.themes.PreviewTheme
+import com.jooheon.clean_architecture.presentation.utils.UiText
 
-
+private const val TAG = "PlayerScreen"
+@ExperimentalPermissionsApi
 @Composable
 fun SearchScreen() {
+    // ExoPlayer 정리글
+    // https://jungwoon.github.io/android/library/2020/11/06/ExoPlayer.html
+
+    val context = LocalContext.current
+    val exoPlayer = remember { createExoPlayer(context) }
+    var contentsFlag by remember { mutableStateOf(false) }
+    setMediaContent(context, exoPlayer, contentsFlag)
+
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 10.dp)
+            .background(MaterialTheme.colorScheme.background),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        Text(text = "SearchScreen")
+
+        Text(
+            text = "This is ExoPlayer",
+            style = MaterialTheme.typography.labelLarge,
+            color = MaterialTheme.colorScheme.onBackground
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        OutlinedButton(
+            modifier = Modifier,
+            onClick = {
+                setMediaContent(context, exoPlayer, contentsFlag)
+                contentsFlag = !contentsFlag
+            },
+            content = {
+                Text(
+                    text = "Toggle to " + if(contentsFlag) { "mp3" } else { "mp4" },
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.onBackground
+                )
+            },
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        AndroidView(
+            modifier = Modifier
+                .fillMaxWidth()
+                .heightIn(
+                    min = 100.dp,
+                    max = 400.dp
+                ),
+            factory = {
+                StyledPlayerView(it).apply {
+                    player = exoPlayer
+                }
+            }
+        )
+    }
+
+    ObserveLifecycleEvent(exoPlayer)
+}
+
+private fun setMediaContent(context: Context, exoPlayer: ExoPlayer, flag: Boolean) {
+    val mediaContent: String
+    if(flag) {
+        mediaContent = UiText.StringResource(R.string.media_url_mp4).asString(context)
+    } else {
+        mediaContent = UiText.StringResource(R.string.test_mp3).asString(context)
+    }
+
+    exoPlayer.apply {
+        val dataSourceFactory = DefaultHttpDataSource.Factory().apply {
+            setUserAgent(Util.getUserAgent(context, context.packageName))
+        }
+        val mediaSource = ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(
+            MediaItem.fromUri(mediaContent)
+        )
+
+        clearMediaItems()
+        addMediaSource(mediaSource)
+        prepare()
+    }
+}
+
+private fun getMediaItem(context: Context, resourceId: Int) {
+    val resource = UiText.StringResource(resourceId).asString(context)
+    val mediaItem = MediaItem.fromUri(resource)
+
+}
+
+private fun createExoPlayer(context: Context) = ExoPlayer.Builder(context).build()
+    .apply { addListener(listener) }
+
+@ExperimentalPermissionsApi
+@Composable
+private fun ObserveLifecycleEvent(
+    exoPlayer: ExoPlayer
+) {
+    val lifecycleOwner by rememberUpdatedState(LocalLifecycleOwner.current)
+    DisposableEffect(
+        key1 = lifecycleOwner,
+        effect = {
+            val observer = LifecycleEventObserver { lifecycleOwner, event ->
+                if(event == Lifecycle.Event.ON_STOP) {
+                    exoPlayer.release()
+                }
+            }
+
+            lifecycleOwner.lifecycle.addObserver(observer)
+
+            onDispose {
+                lifecycleOwner.lifecycle.removeObserver(observer)
+            }
+        }
+    )
+}
+
+private val listener = object : Player.Listener {
+    override fun onPlaybackStateChanged(playbackState: Int) {
+        super.onPlaybackStateChanged(playbackState)
+        when (playbackState) {
+            Player.STATE_IDLE -> {
+                Log.d(TAG, "onPlaybackStateChanged: Player.STATE_IDLE")
+            }
+            Player.STATE_BUFFERING -> {
+                Log.d(TAG, "onPlaybackStateChanged: Player.STATE_BUFFERING")
+            }
+            Player.STATE_READY -> {
+                Log.d(TAG, "onPlaybackStateChanged: Player.STATE_READY")
+            }
+            Player.STATE_ENDED -> {
+                Log.d(TAG, "onPlaybackStateChanged: Player.STATE_ENDED")
+            }
+        }
+    }
+
+    override fun onIsPlayingChanged(isPlaying: Boolean) {
+        super.onIsPlayingChanged(isPlaying)
+        Log.d(TAG, "onIsPlayingChanged: $isPlaying")
+    }
+
+    override fun onMediaMetadataChanged(mediaMetadata: MediaMetadata) {
+        super.onMediaMetadataChanged(mediaMetadata)
+        Log.d(TAG, "onMediaMetadataChanged title: ${mediaMetadata.title}, displayTtle: ${mediaMetadata.displayTitle}")
     }
 }
 
 @Preview
 @Composable
 fun PreviewSearchScreen() {
-    SearchScreen()
+    PreviewTheme(false) {
+        SearchScreen()
+    }
 }
