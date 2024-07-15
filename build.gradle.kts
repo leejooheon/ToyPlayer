cddb8a3c50695361a3928c3cd6411f732119fc01
Jooheon <jooheon.lee@bugs.co.kr>
2024-07-15 11:18:25 +0900

    🔧Chore: update libraries, kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ buildscript {
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.hilt) apply false
