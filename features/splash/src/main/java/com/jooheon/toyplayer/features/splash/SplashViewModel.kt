b01642752b66bc400549d5f1a7060470e8904542
Jooheon <jooheon.lee@bugs.co.kr>
2025-03-28 13:22:42 +0900

    Fix: default playlist error

--- features/splash/src/main/java/com/jooheon/toyplayer/features/splash/SplashViewModel.kt
+++ features/splash/src/main/java/com/jooheon/toyplayer/features/splash/SplashViewModel.kt
@@ -3,19 +3,21 @@ package com.jooheon.toyplayer.features.splash
 import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.jooheon.toyplayer.domain.model.music.MediaId
+import com.jooheon.toyplayer.features.musicservice.player.PlayerController
 import com.jooheon.toyplayer.features.splash.model.SplashState
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.suspendCancellableCoroutine
 import javax.inject.Inject
-import kotlin.time.Duration.Companion.milliseconds
+import kotlin.coroutines.resume
 
 @HiltViewModel
-class SplashViewModel @Inject constructor(): ViewModel() {
+class SplashViewModel @Inject constructor(
+    private val playerController: PlayerController
+): ViewModel() {
     private val _state = Channel<SplashState>()
     val state = _state.receiveAsFlow()
 
@@ -25,12 +27,14 @@ class SplashViewModel @Inject constructor(): ViewModel() {
         }
     }
 
-    private fun prepareMediaItems(context: Context) {
-        viewModelScope.launch {
-            withContext(Dispatchers.IO) {
-                delay(100.milliseconds) // do something
-            }
-            _state.send(SplashState.Done)
+    private suspend fun prepareMediaItems(context: Context) {
+        suspendCancellableCoroutine { continuation ->
+            playerController.getMusicListFuture(
+                context = context,
+                mediaId = MediaId.Root,
+                listener = { continuation.resume(it) }
+            )
         }
+        _state.send(SplashState.Done)
     }
 }
