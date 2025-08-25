b021ca49877aa90df260bf4c8f3067a5e94fa9f5
Jooheon <jooheon.lee@bugs.co.kr>
2025-08-25 13:29:16 +0900

    style: fix ui bug

--- features/player/src/main/java/com/jooheon/toyplayer/features/player/component/info/InfoSection.kt
+++ features/player/src/main/java/com/jooheon/toyplayer/features/player/component/info/InfoSection.kt
@@ -4,13 +4,16 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.pager.PagerDefaults
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.VerticalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
@@ -19,10 +22,16 @@ import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.integerResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.rememberTextMeasurer
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import com.jooheon.toyplayer.core.designsystem.theme.ToyPlayerTheme
 import com.jooheon.toyplayer.core.resources.Strings
@@ -30,12 +39,10 @@ import com.jooheon.toyplayer.core.resources.UiText
 import com.jooheon.toyplayer.domain.model.common.extension.defaultEmpty
 import com.jooheon.toyplayer.domain.model.music.Playlist
 import com.jooheon.toyplayer.domain.model.music.Song
-import com.jooheon.toyplayer.features.commonui.ext.toPx
+import com.jooheon.toyplayer.features.commonui.ext.toDp
 import com.jooheon.toyplayer.features.musicservice.data.MusicState
-import com.jooheon.toyplayer.features.player.common.cardBottomPreviewHeight
-import com.jooheon.toyplayer.features.player.common.cardTopPreviewHeight
+import com.jooheon.toyplayer.features.player.common.contentHeight
 import com.jooheon.toyplayer.features.player.common.contentSize
-import com.jooheon.toyplayer.features.player.common.contentSpace
 import com.jooheon.toyplayer.features.player.component.info.content.ContentSection
 import com.jooheon.toyplayer.features.player.component.info.control.ControlSection
 import com.jooheon.toyplayer.features.player.model.PlayerUiState
@@ -112,12 +119,7 @@ fun InfoSection(
                     modifier = Modifier
                         .fillMaxSize()
                         .zIndex(if (pageIndex == 0) Float.MAX_VALUE else 0f)
-                        .graphicsLayer(
-                            translationY = calculateYOffset(
-                                state = pagerState,
-                                index = pageIndex,
-                            )
-                        )
+                        .graphicsLayer(translationY = calculateYOffset(pageIndex, pagerState))
                 ) {
                     when (pageIndex) {
                         0 -> ControlSection(
@@ -134,7 +136,7 @@ fun InfoSection(
                             onNextClick = onNextClick,
                             onPreviousClick = onPreviousClick,
                             onSeek = onSeek,
-                            modifier = Modifier
+                            modifier = Modifier.windowInsetsPadding(WindowInsets.safeDrawing)
                         )
                         else -> {
                             val isLastPage = pagerState.currentPage == pagerState.pageCount - 1
@@ -169,16 +171,45 @@ fun InfoSection(
 }
 
 @Composable
-private fun calculateYOffset(state: PagerState, index: Int): Float {
-    // NOTE
-    // 실제로 보여야하는 부분은 cardTopPreviewHeight 인데
-    // item 상단에 cardBottomPreviewHeight, contentSpace 가 상단에 있으므로 이것도 더해야함
-    // item = (ContentPagerItem, ContentScrollableItem)
-    val heightPx = (cardTopPreviewHeight() + cardBottomPreviewHeight() + contentSpace()).toPx()
-    val pageOffset = state.currentPage - index + state.currentPageOffsetFraction
-    val offset = pageOffset * -heightPx
-//    Timber.d("calculateYOffset: $offset, $heightPx")
+private fun calculateYOffset(page: Int, pagerState: PagerState): Float {
+    val pageOffset = pagerState.currentPage - page + pagerState.currentPageOffsetFraction
+    val offset = pageOffset * -calculateOffsetBetweenPages()
     return -offset
+}
+
+@Composable
+private fun calculateOffsetBetweenPages(): Int {
+    val offsetBetweenPages = with(LocalDensity.current) {
+        val height = (contentHeight() * 0.4f).toPx() // 카드 height의 40%만 보여야함
+        height + wholeContentSectionHeight().toPx()
+    }
+    val safeDrawingTop = WindowInsets.safeDrawing.getTop(LocalDensity.current)
+    return offsetBetweenPages.toInt() - safeDrawingTop
+}
+
+@Composable
+private fun wholeContentSectionHeight(): Dp {
+    val titleContent =  textContentHeight(MaterialTheme.typography.bodyLarge).toDp()
+    val subtitleContent =  textContentHeight(MaterialTheme.typography.bodySmall).toDp()
+    val paddingBottom = (contentHeight() * 0.6f)
+    return titleContent + subtitleContent + 48.dp + 44.dp + paddingBottom
+}
+
+@Composable
+internal fun textContentHeight(
+    style: TextStyle,
+    maxLines: Int = 2,
+): Int {
+    val textMeasurer = rememberTextMeasurer()
+    val textLayoutResult = textMeasurer.measure(
+        text = UiText.StringResource(Strings.lorem_ipsum).asString(),
+        style = style,
+        maxLines = maxLines,
+        overflow = TextOverflow.Ellipsis,
+        constraints = Constraints.fixedWidth(1)
+    )
+    val textHeight = textLayoutResult.size.height
+    return textHeight
 }
 
 @Preview(
